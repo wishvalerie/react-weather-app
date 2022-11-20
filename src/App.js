55eba4f043ea364e594d46a06e401d6a6a69065c
wishvalerie <wishvaleriee@gmail.com>
2022-11-20 12:55:58 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="" target="_blank">
+            Valeriia Yarovyk
+          </a>{" "}
+          and is{" "}
+          <a href="" target="_blank">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
